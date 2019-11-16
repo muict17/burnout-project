e4fc5fe902f786fcf69aabff8c60e7efab9e41bc
jarupong pajakgo <lagmanmc@gmail.com>
2019-11-16 23:39:55 +0700

    [Improvement, Logger] add logger

--- backend/src/routes/authentication/register/schema.ts
+++ backend/src/routes/authentication/register/schema.ts
@@ -10,5 +10,50 @@ export default {
       }
     },
     required: ["username", "password"]
+  },
+  response: {
+    200: {
+      type: "object",
+      properties: {
+        msg: {
+          type: "string"
+        },
+        userInfo: {
+          type: "object",
+          properties: {
+            username: {
+              type: "string"
+            },
+            createAt: {
+              type: "string",
+              format: "date-time"
+            },
+            updateAt: {
+              type: "string",
+              format: "date-time"
+            },
+            userId: {
+              type: "string"
+            }
+          }
+        }
+      }
+    },
+    409: {
+      type: "object",
+      properties: {
+        msg: {
+          type: "string"
+        }
+      }
+    },
+    500: {
+      type: "object",
+      properties: {
+        msg: {
+          type: "string"
+        }
+      }
+    }
   }
 };
