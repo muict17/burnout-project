292baa94012ea3fe4c8e2e1de8095f69c5d03dd9
benzdechapon <dechapon.t@gmail.com>
2019-12-23 14:52:38 +0700

    Update the layout

--- frontend/src/views/Admin/Home.vue
+++ frontend/src/views/Admin/Home.vue
@@ -1,36 +1,75 @@
 <template>
   <div>
     <Navbar />
-    <h3>Create Movie</h3>
-    Title : <input type="text" v-model="movieName" /> <br />
-    Price : <input type="text" v-model="moviePrice" /> <br />
-    <button @click="addMovie">ADD</button><br /><br />
+    
+    <div class="container">
+      <h1 class="title">Create Movie</h1>
+      Title : <input type="text" v-model="movieName" /> <br />
+      Price : <input type="text" v-model="moviePrice" /> <br />
+      <input type="file" @change="onFileSelected" > <br />
+      <button @click="addMovie" class="button is-info">ADD</button><br /><br />
 
-    <div v-for="(element, index) in listOfMovie" :key="index">
-      <div>
-        {{ element.movie }}
-        {{ element.price }}
-        <button class="button is-info" @click="deleteMovie(index)">
-          Delete the Movie
-        </button>
-        <button class="button is-info" @click="toggleEdit(index)">
-          Edit the Movie
-        </button>
-        <br />
-      </div>
-      {{ listOfMovie }}
-      <div v-if="toggle[index]">
-        Title : <input type="text" v-model="element.movie" />
-        <br />
-        Price :
-        <input type="text" v-model="element.price" />
-        <button
-          class="button is-info"
-          @click="clickEdit(index, element.movie, element.price)"
-        >
-          Confirm
-        </button>
-      </div>
+    <h1 class="title">Movies List</h1>
+    <table class="table is-fullwidth">
+      
+      <tr>
+        <th>Image</th>
+        <th>Movie name</th>
+        <th>Price</th>
+        <th>Delete Movie</th>
+        <th>Edit Movie</th>
+      </tr>
+        
+        <tr v-for="(element, index) in listOfMovie" :key="index">   
+          <td><img :src="element.imgFile"></td>
+          <td>{{ element.movie }}</td>
+          <td>{{ element.price }}</td>
+          <td><button class="button is-info" @click="deleteMovie(index)">
+            Delete the Movie
+          </button></td>
+          <td><button class="button is-info" @click="toggleEdit(index)">
+            Edit the Movie
+          </button>
+          <div v-if="toggle[index]">
+            
+          <br>
+          Title : <input type="text" v-model="element.movie" /> <br />
+          Price : <input type="text" v-model="element.price" /> <br />
+          Image : <input type="file" @change="onFileSelected" > <br />
+          <button
+            class="button is-info"
+            @click="clickEdit(index, element.movie, element.price, element.imgFile)"
+          >
+            Confirm
+          </button>
+         
+        </div>
+
+          </td>
+<!--           
+          <div v-if="toggle[index]">
+            
+          <br>
+          Title : <input type="text" v-model="element.movie" />
+          <br />
+          Price :
+          <input type="text" v-model="element.price" />
+          
+          <button
+            class="button is-info"
+            @click="clickEdit(index, element.movie, element.price)"
+          >
+            Confirm
+          </button>
+         
+        </div> -->
+          
+          </tr>
+
+        
+        
+      
+      </table>
     </div>
   </div>
 </template>
@@ -42,6 +81,7 @@ export default {
       listOfMovie: [],
       movieName: "",
       moviePrice: "",
+      selectedfile: null,
       toggle: []
     };
   },
@@ -50,19 +90,28 @@ export default {
     Navbar
   },
   methods: {
+    onFileSelected(event){
+      /* eslint-disable */
+      console.log(event)
+      this.selectedfile = event.target.files[0]
+    },
     addMovie() {
-      this.listOfMovie.push({ movie: this.movieName, price: this.moviePrice });
+      this.listOfMovie.push({ movie: this.movieName, price: this.moviePrice, imgFile: this.selectedfile });
       this.toggle.push(false);
+       /* eslint-disable */
+       console.log(this.selectedfile);
     },
 
     deleteMovie(index) {
       this.listOfMovie.splice(index, 1);
     },
-    clickEdit(index, name, price) {
+    clickEdit(index, name, price,imgFile) {
       /* eslint-disable */
       console.log(index, name, price);
       this.listOfMovie[index].movie = name;
       this.listOfMovie[index].price = price;
+      this.listOfMovie[index].imgFile = imgFile;
+      this.$set(this.toggle, index, !this.toggle[index]);
     },
     toggleEdit(index) {
       this.$set(this.toggle, index, !this.toggle[index]);
@@ -70,3 +119,20 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+
+.container > *{
+  margin-top: 3px;
+}
+
+.title{
+  font-size: 18px;
+  margin: 0px;
+  margin-top: 10px;
+
+}
+
+
+
+</style>
